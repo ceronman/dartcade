78e026717a51d8b4d1223add7087dc94601426a1
Manuel Cerón <ceronman@gmail.com>
2012-08-17 22:59:38 -0500

    Improved place action test.

--- tests/test_action_place.dart
+++ tests/test_action_place.dart
@@ -6,9 +6,6 @@ void main() {
   var layer = new Layer();
   var label = new Label('Place!');
 
-  label.position.x = 100;
-  label.position.y = 100;
-
   layer.add(label);
   print('Old position: ${label.position}');
   label.runAction(new Place(new vec2(200, 200)));
