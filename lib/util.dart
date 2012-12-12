f0ea5a242991f5364b468a256bb3c36e397ed491
Manuel Cerón <ceronman@gmail.com>
2012-12-11 19:00:11 -0500

    Improved copyright notes.

--- lib/util.dart
+++ lib/util.dart
@@ -1,5 +1,3 @@
-part of cocos;
-
 // Copyright 2012 Manuel Cerón <ceronman@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,6 +11,9 @@ part of cocos;
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+part of cocos;
+
 class EventListeners implements EventListenerList {
   var listeners;
 
