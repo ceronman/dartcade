35b3a89940486493e9fe4b6f9f996a7dd848b7b2
Manuel Ceron <manuel.ceron@booking.com>
2015-04-22 17:08:26 -0500

    Pong demo: Added a few missing collisions.

--- example/pong/pong.dart
+++ example/pong/pong.dart
@@ -43,7 +43,7 @@ main() {
       ..runAction(controller2);
 
     var ballbody = new ArcadeBody()
-      ..speed = new Vector2(-300.0, 400.0)
+      ..speed = new Vector2(-300.0, 200.0)
       ..restitution = new Vector2(1.0, 1.0)
       ..addTo(world);
 
@@ -60,11 +60,13 @@ main() {
     wallsCollision.collider1 = ballbody.collider;
     wallsCollision.collider2 = world.collider;
     wallsCollision.onCollision.listen((event) {
-      if (event.delta.normalized().x != 0.0 && event.delta.normalized().y != 0.0) {
-        print("walls ${event.delta}");
-      }
       ballbody.position.add(event.delta);
-      ballbody.speed.reflect(event.delta.normalized());
+      if (event.delta.x != 0.0) {
+        ballbody.speed.reflect(new Vector2(event.delta.x.sign, 0.0));
+      }
+      if (event.delta.y != 0.0) {
+        ballbody.speed.reflect(new Vector2(0.0, event.delta.y.sign));
+      }
       ballbody.syncToNode();
     });
     world.collisions.add(wallsCollision);
@@ -84,14 +86,35 @@ main() {
     body2.collider = new AABB2Collider();
     body2.collider.body = body2;
     var paddle2Collision = new StaticBodyVsBodyCollisionCheck()
-          ..collider1 = body2.collider
-          ..collider2 = ballbody.collider;
+      ..collider1 = body2.collider
+      ..collider2 = ballbody.collider;
     paddle2Collision.onCollision.listen((event) {
       ballbody.position.add(event.delta);
       ballbody.speed.reflect(event.delta.normalized());
       ballbody.syncToNode();
     });
     world.collisions.add(paddle2Collision);
+
+    var paddle1wallCollision = new StaticBodyOutOfBoundsCollisionCheck()
+      ..collider1 = body1.collider
+      ..collider2 = world.collider;
+
+    paddle1wallCollision.onCollision.listen((event) {
+      body1.position.add(event.delta);
+      body1.syncToNode();
+    });
+
+    var paddle2wallCollision = new StaticBodyOutOfBoundsCollisionCheck()
+      ..collider1 = body2.collider
+      ..collider2 = world.collider;
+
+    paddle2wallCollision.onCollision.listen((event) {
+      body2.position.add(event.delta);
+      body2.syncToNode();
+    });
+
+    world.collisions.add(paddle1wallCollision);
+    world.collisions.add(paddle2wallCollision);
   });
 
   game.start();
